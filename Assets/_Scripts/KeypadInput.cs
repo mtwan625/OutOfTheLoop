f0da50a6f354e22a45ab5168cc8fefa3bcb29766
Kyle Nguyen <kylenguyen7@users.noreply.github.com>
2019-11-13 14:18:14 -0800

    Heard Y'all Like Keypads 2
    Added VR integration for Keypad scene. Also, built to VR so... not sure if that changes anything on your ends.

--- Assets/_Scripts/KeypadInput.cs
+++ Assets/_Scripts/KeypadInput.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class KeypadInput : MonoBehaviour
 {
     public string character;
+
+    private RaycastHit hit;
+    private Vector3 direction = Vector3.up;
+    private float distance = 3;
+
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, direction * 3, Color.green);
+
+        if (Physics.Raycast(transform.position, direction, out hit, distance * 3))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
 }
