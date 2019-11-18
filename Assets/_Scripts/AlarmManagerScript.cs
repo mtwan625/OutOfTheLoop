3f9a1ab486b84b000f5c2d2cde3eb264cf9fcc8d
steelejas <steele.jasmine.d@gmail.com>
2019-11-17 17:39:53 -0800

    reimported assets after updating
    re-imported/re-added models, materials, and AlarmManagerScript, no more compiler errors but script doesn't properly recognize alarms 1, 2, 3, and the alarm on/off materials

---
+++ Assets/_Scripts/AlarmManagerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmManagerScript : MonoBehaviour
+{
+    bool alarm1On = false;
+    bool alarm2On = false;
+    bool alarm3On = false;
+    int nextAlarm = 1;
+
+    public GameObject alarm1;
+    public GameObject alarm2;
+    public GameObject alarm3;
+
+    Material on;
+    Material off;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alarm1On = false;
+    	alarm2On = false;
+    	alarm3On = false;
+    	nextAlarm = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void triggerAlarm() {
+    	switch (nextAlarm) {
+    		case 1:
+    			alarm1On = true;
+    			alarm1.GetComponent<Renderer>().material = on;
+    			break;
+    		case 2:
+    			alarm2On = true;
+    			alarm2.GetComponent<Renderer>().material = on;
+    			break;
+    		case 3:
+    			alarm3On = true;
+    			alarm3.GetComponent<Renderer>().material = on;
+    			break;
+    		default:
+    			break;
+    	}
+    	nextAlarm++;
+    	// if (alarm1On && alarm2On && alarm3On) { play alarm sound? }
+    }
+
+    void disableAlarms() {
+    	alarm1On = false;
+    	alarm2On = false;
+    	alarm3On = false;
+    	alarm1.GetComponent<Renderer>().material = off;
+    	alarm2.GetComponent<Renderer>().material = off;
+    	alarm3.GetComponent<Renderer>().material = off;
+    	nextAlarm = 1;
+    }
+}
