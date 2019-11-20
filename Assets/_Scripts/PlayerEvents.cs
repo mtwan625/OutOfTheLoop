32fd6566833c2131da7774eee0d7979fd0d03860
Michelle Wan <mtwan625@users.noreply.github.com>
2019-11-19 22:19:39 -0800

    Implement pointer
    Template for pointer interaction in Pointer.cs (only for touchpad)

---
+++ Assets/_Scripts/PlayerEvents.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerEvents : MonoBehaviour
+{
+    public static UnityAction OnTouchpadUp = null;
+    public static UnityAction OnTouchpadDown = null;
+    public static UnityAction<OVRInput.Controller, GameObject> OnControllerSource = null;
+
+    public GameObject m_RightAnchor;
+
+    private OVRInput.Controller m_InputSource = OVRInput.Controller.None;
+    private OVRInput.Controller m_Controller = OVRInput.Controller.None;
+    private bool m_InputActive = true;
+
+    void Awake()
+    {
+        OVRManager.HMDMounted += PlayerFound;
+        OVRManager.HMDUnmounted += PlayerLost;
+    }
+
+    void OnDestroy()
+    {
+        OVRManager.HMDMounted -= PlayerFound;
+        OVRManager.HMDUnmounted -= PlayerLost;
+    }
+
+    void Update()
+    {
+        // Check for active input
+        if (!m_InputActive)
+            return;
+
+        // Check if a controller exists
+        CheckForController();
+
+        // Check for input source
+        CheckInputSource();
+
+        // Check for actual input
+        Input();
+    }
+
+    void CheckForController()
+    {
+        OVRInput.Controller controllerCheck = m_Controller;
+
+        // Right remote
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote))
+            controllerCheck = OVRInput.Controller.RTrackedRemote;
+
+        m_Controller = UpdateSource(controllerCheck, m_Controller);
+    }
+
+    void CheckInputSource()
+    {
+        m_InputSource = UpdateSource(OVRInput.GetActiveController(), m_InputSource);
+    }
+
+    void Input()
+    {
+        // Touchpad down
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadDown != null)
+                OnTouchpadDown();
+        }
+
+        // Touchpad up
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadUp != null)
+                OnTouchpadUp();
+        }
+    }
+
+    OVRInput.Controller UpdateSource(OVRInput.Controller check, OVRInput.Controller previous)
+    {
+        if (check == previous)
+            return previous;
+
+        GameObject controllerObject = m_RightAnchor;
+
+        if (OnControllerSource != null)
+            OnControllerSource(check, controllerObject);
+
+        return check;
+    }
+
+    void PlayerFound()
+    {
+        m_InputActive = true;
+    }
+
+    void PlayerLost()
+    {
+        m_InputActive = false;
+    }
+}
