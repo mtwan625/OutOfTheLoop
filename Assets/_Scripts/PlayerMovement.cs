973b0a716202eac21b44e96a4b628d359450b37e
Michelle Wan <mtwan625@users.noreply.github.com>
2019-11-20 02:42:52 -0800

    Revert "Delete PlayerMovement script"
    This reverts commit c68176239ce95377295067f55d1ced1592e85dd4.

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public GameObject cameraRig;
+    public GameObject centerEye;
+    
+    void Update()
+    {
+        Vector2 move = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+
+        transform.eulerAngles = new Vector3(0, centerEye.transform.localEulerAngles.y, 0);
+        transform.Translate(Vector3.forward * speed * move.y * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * move.x * Time.deltaTime);
+        
+        cameraRig.transform.position = transform.position;
+    }
+}
